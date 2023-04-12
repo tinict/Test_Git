7eb236bcb42eae9904aa2e29efb954b998b5d5bd
Tín Nguyễn <89054327+tinict@users.noreply.github.com>
2023-04-12 21:21:57 +0700

    Update

--- View/Templates/Home/Header.js
+++ View/Templates/Home/Header.js
@@ -10,11 +10,17 @@ export const Header = () => {
         <header class="mod_header">
             <div class="header__container">
                 <div class="header__left">
+                <div class='icon'>
+                    <i class="fas fa-bars"></i>
+                </div>
                     <div class="header__brand">
                         <img src="assets/img/logo_laptop-home.png" alt="logo company">
                     </div>
                 </div>
                 <div class="header__center">
+                    <div class="header__brand">
+                        <img src="assets/img/logo_laptop-home.png" alt="logo company">
+                    </div>
                     ${Search()}
                     ${NavigationBar()}
                 </div>
@@ -24,4 +30,12 @@ export const Header = () => {
             </div>
         </header>
     `
+};
+
+export const SubHeader = () => {
+    return `
+        <div class="sub_header">
+            ${Search()}
+        </div>
+    `;
 };
--- View/app.js
+++ View/app.js
@@ -1,12 +1,13 @@
 "use strict";
 
 import { newElement } from "../Lib/VieStar/Core.js";
-import { Header } from "./Templates/Home/Header.js"
+import { Header, SubHeader } from "./Templates/Home/Header.js"
 
 const app = () => {
     return newElement`
         <div class='container'>
             ${Header()}
+            ${SubHeader()}
         </div>
     `
 };
